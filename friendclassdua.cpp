b3e19f1763befebc9970a074b068f4662e6c48eb
aswarina <lyxsyyawa@gmail.com>
2026-04-28 20:05:06 +0700

    jalankan logika penampilan nilai melalui objek manusia

--- friendclassdua.cpp
+++ friendclassdua.cpp
@@ -17,3 +17,15 @@ public:
     pelajar() { nilai = 100; }
     friend void manusia::showNilaipelajar(pelajar &x);
 };
+void manusia::showNilaipelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaipelajar(pbudi);
+    return 0;
+}
