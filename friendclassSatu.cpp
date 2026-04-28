b3bbd3d5a58708212886c5044fdc384492c029c6
aswarina <lyxsyyawa@gmail.com>
2026-04-28 19:51:46 +0700

    perbaikan sintaks dan penambahhan fungsi friend class

--- friendclassSatu.cpp
+++ friendclassSatu.cpp
@@ -2,30 +2,54 @@
 #include <string>
 using namespace std;
 
-class siswa;
-class out_of_range
+class siswa; // Forward declaration
+class orang
 {
 private:
-   string nama;
+    string nama;
 
 public:
-   void setNama(string pNama);
-   friend class siswa;
-
+    void setNama(string pNama);
+    friend class siswa; // Memberi izin class siswa akses data private
 };
 
 class siswa
 {
 private:
-   int id;
+    int id;
 
 public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
 // Implementasi method displayAll
 void siswa::displayAll(orang &a)
 {
     cout << id << endl << a.nama << endl;
 }
 
+// Implementasi method setNama
+void orang::setNama(string pNama)
+{
+    nama = pNama; // Tadi kurang titik koma (;)
+}
+
+// Implementasi method setId
+void siswa::setId(int pId) // Tadi nId tapi di dalamnya pId, disamakan jadi pId
+{
+    id = pId;
+}
+
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    
+    joko_siswa.displayAll(joko);
+    
+    return 0; // Tadi titik dua (:), diganti titik koma (;)
+}
